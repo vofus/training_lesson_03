56dfe7ce948db847a1bc356ff04704999b9a22d0
Andrey Vasin <vofuss@gmail.com>
2016-07-13 23:39:14 +0400

    Fix returned functions

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // myConcat function module
-var myConcatModule = (function() {
+var myConcat = (function() {
 	
-	return {
-		myConcat: function() {
+	return function() {
 			var args = arguments,
 				argsLength = arguments.length,
 				argsType = "",
@@ -69,96 +68,91 @@ var myConcatModule = (function() {
 				return false;
 			}
 		}
-	}
 
 })();
 
 // convertNums function module
-var convertNumsModule = (function() {
+var convertNums = (function() {
 
-	return {
-		convertNums: function() {
-			var args = arguments,
-				argsLength = arguments.length,
-				originalNum,
-				returnedResult;
+	return  function() {
+				var args = arguments,
+					argsLength = arguments.length,
+					originalNum,
+					returnedResult;
 
-			if(argsLength === 0) {
-				console.log("Ошибка! Вы не ввели ни одного аргумента.");
-				return false;
-			} else {
-				originalNum = args[0];
+				if(argsLength === 0) {
+					console.log("Ошибка! Вы не ввели ни одного аргумента.");
+					return false;
+				} else {
+					originalNum = args[0];
+				}
+
+				if(typeof(originalNum) === "number") {
+					returnedResult = Math.round(originalNum * 100) / 100;
+					returnedResult = returnedResult.toLocaleString();
+					return returnedResult;
+				} else {
+					console.log("Ошибка! Вы ввели не число.");
+					return false;
+				}
 			}
-
-			if(typeof(originalNum) === "number") {
-				returnedResult = Math.round(originalNum * 100) / 100;
-				returnedResult = returnedResult.toLocaleString();
-				return returnedResult;
-			} else {
-				console.log("Ошибка! Вы ввели не число.");
-				return false;
-			}
-	;	}
-	}
 
 })();
 
 // checkSubStr function module
-var checkSubStrModule = (function() {
+var checkSubStr = (function() {
 
-	return {
-		checkSubStr: function() {
-			var args = arguments,
-				argsLength = arguments.length,
-				testArr = [],
-				searchSubStr,
-				returnedResult;
+	return function() {
+				var args = arguments,
+					argsLength = arguments.length,
+					testArr = [],
+					searchSubStr,
+					returnedResult;
 
-			if(argsLength < 2) {
-				console.log("Ошибка! Необходимо ввести 2 аргумента.");
-				return false;
-			} else {
-				if(Array.isArray(args[0])) {
-					if(args[0].length !== 0) {
-						for(var i=0; i<args[0].length; i++) {
-							if(typeof(args[0][i]) === "string") {
-								testArr = args[0];
-							} else {
-								console.log("Ошибка! Все элементы входного массива должны быть строками.");
-								return false;
+				if(argsLength < 2) {
+					console.log("Ошибка! Необходимо ввести 2 аргумента.");
+					return false;
+				} else {
+					if(Array.isArray(args[0])) {
+						if(args[0].length !== 0) {
+							for(var i=0; i<args[0].length; i++) {
+								if(typeof(args[0][i]) === "string") {
+									testArr = args[0];
+								} else {
+									console.log("Ошибка! Все элементы входного массива должны быть строками.");
+									return false;
+								}
 							}
-						}
+						} else {
+							console.log("Ошибка! Массив не должен быть пустым.");
+							return false;
+						}	
 					} else {
-						console.log("Ошибка! Массив не должен быть пустым.");
+						console.log("Ошибка! Первый аргумент не является массивом.");
 						return false;
-					}	
-				} else {
-					console.log("Ошибка! Первый аргумент не является массивом.");
-					return false;
+					}
+					if(typeof(args[1]) === "string") {
+						searchSubStr = new RegExp(args[1], "ig");
+					} else {
+						console.log("Ошибка! Второй аргумент не является строкой.");
+						return false;
+					}
 				}
-				if(typeof(args[1]) === "string") {
-					searchSubStr = new RegExp(args[1], "ig");
-				} else {
-					console.log("Ошибка! Второй аргумент не является строкой.");
-					return false;
+
+				for(var i=0; i < testArr.length; i++) {
+					var str = testArr[i];
+					if(searchSubStr.test(str)) {
+						returnedResult = true;
+						break;
+					} else {
+						returnedResult = false;
+						continue;
+					}
 				}
+
+				return returnedResult;
+
 			}
-
-			for(var i=0; i < testArr.length; i++) {
-				var str = testArr[i];
-				if(searchSubStr.test(str)) {
-					returnedResult = true;
-					break;
-				} else {
-					returnedResult = false;
-					continue;
-				}
-			}
-
-			return returnedResult;
-
-		}
-	}
 
 })();
 
